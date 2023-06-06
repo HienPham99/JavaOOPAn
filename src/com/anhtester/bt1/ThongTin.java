9b0341146322dd143d444b0ecfe597283a4f817c
HienPham99 <qctest898@gmail.com>
2023-06-06 18:22:44 +0700

    update source code bt1 OOP

---
+++ src/com/anhtester/bt1/ThongTin.java
@@ -0,0 +1,11 @@
+package com.anhtester.bt1;
+
+public class ThongTin {
+
+
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        System.out.println(employee.name);
+        System.out.println(employee.address);
+    }
+}
